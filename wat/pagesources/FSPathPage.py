3bb749898b3a831acb119d6a55b330513dbc4baa
Patrick R <hi@patrickrein.de>
2023-02-05 20:00:41 +0100

    Reworks the Path page to only load the pages when needed

--- wat/pagesources/FSPathPage.py
+++ wat/pagesources/FSPathPage.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Dict
 from .AbstractPage import AbstractPage
 import json
 import os
@@ -6,8 +6,7 @@ import os
 
 class FSPathPage(AbstractPage):
 
-    with open("./fspages.json") as pages_file:
-        pages: Dict[str, str] = json.load(pages_file)
+    pages = {}
 
     def __init__(self, path):
         self.path = path
@@ -18,11 +17,18 @@ class FSPathPage(AbstractPage):
 
     @classmethod
     def has_page(cls, path) -> bool:
-        return cls.is_path(path) and path in cls.pages
+        return cls.is_path(path) and path in cls.all_pages()
 
     @classmethod
     def get_page(cls, path) -> 'FSPathPage':
         return cls(path)
+
+    @classmethod
+    def all_pages(cls) -> Dict[str, str]:
+        if not cls.pages:
+            with open("./fspages.json") as pages_file:
+                cls.pages = json.load(pages_file)
+        return cls.pages
 
     def description(self, detailed=False) -> str:
         return FSPathPage.all_pages().get(self.path, "no page found")
