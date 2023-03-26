7d792e4205aa591fc4a2aa6a8b37232a47e3b031
Patrick R <hi@patrickrein.de>
2023-03-26 16:29:40 +0200

    Updates the FSPathPage tests

--- tests/test_fspathpage.py
+++ tests/test_fspathpage.py
@@ -3,32 +3,6 @@ from pyfakefs.fake_filesystem_unittest import Patcher
 
 from wat.pagesources import FSPathPage
 
-#
-# Has Page
-#
-
-def test_has_path_for_absolute_path_directory():
-    assert FSPathPage.has_page("/etc")
-    assert not FSPathPage.has_page("no_such_path")
-
-
-def test_has_path_for_absolute_path_file():
-    assert FSPathPage.has_page("/etc/hosts")
-    assert not FSPathPage.has_page("no_such_path")
-
-
-def test_has_path_for_relative_path_file():
-    os.chdir('/etc')
-    assert FSPathPage.has_page("hosts")
-
-
-def test_has_page_for_relative_path_in_variable_dir():
-    FSPathPage.has_page("/bin")  # To initialize the pages cache
-    with Patcher() as patcher:
-        patcher.fs.create_file("/home/someUser/.bashrc", contents="test")
-        os.chdir('/home/someUser')
-        assert FSPathPage.has_page(".bashrc")
-
 
 #
 # Get Page
@@ -54,7 +28,6 @@ def test_get_page_for_relative_path_file():
 
 
 def test_get_page_for_relative_path_in_variable_dir():
-    FSPathPage.has_page("/bin")  # To initialize the pages cache
     with Patcher() as patcher:
         patcher.fs.create_file("/home/someUser/.bashrc", contents="test")
         os.chdir('/home/someUser')
