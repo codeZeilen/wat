e92bb91e35240f75fcf4f8039b1478224d6bab0b
Patrick R <hi@patrickrein.de>
2023-06-03 11:48:45 +0200

    Makes tests less dependend on actual implementation

--- tests/test_fspathpage.py
+++ tests/test_fspathpage.py
@@ -2,18 +2,45 @@ import os
 import pathlib
 
 from pyfakefs.fake_filesystem_unittest import Patcher
-import unittest
+from pyfakefs.fake_filesystem_unittest import TestCase
 from unittest.mock import patch
 
 from wat.pagesources import FSPathPage
 from wat.pagesources import GlobTrie
 from wat.pagesources import FileCache
 
-class TestFSPathPage(unittest.TestCase):
+
+class TestFSPathPage(TestCase):
 
     def setUp(self) -> None:
         super().setUp()
+        FileCache.cache_dir_path = lambda: pathlib.Path('/home/someUser/.cache/wat')
         FSPathPage.reset_pages()
+        FSPathPage.initialize_pages()
+
+    @classmethod
+    def create_cached_fs_page(cls, name, content):
+        cls.fake_fs().create_file("/home/someUser/.cache/wat/fs_pages/{}".format(name), 
+          contents=content)   
+
+    @classmethod
+    def setUpClass(cls):
+        cls.setUpClassPyfakefs()
+        trie = GlobTrie()
+        trie.add('/home/*/.bashrc', 'globbashrc.md')
+        trie.add('/bin', 'bin.md')
+        trie.add('/etc/hosts', 'etc-hosts.md')
+        trie.add('/etc', 'etc.md')
+        bashrc_description = 'bashrc description'
+
+        cls.fake_fs().create_file("/home/someUser/.bashrc", contents="test")
+        cls.fake_fs().create_file("/etc/hosts", contents="test")
+        cls.fake_fs().create_dir("/bin")
+        cls.create_cached_fs_page("index.json", trie.store_string())
+        cls.create_cached_fs_page('globbashrc.md', bashrc_description)
+        cls.create_cached_fs_page('bin.md', '---\n---\n\n/bin is a place for most commonly used programs')
+        cls.create_cached_fs_page('etc.md', 'test')
+        cls.create_cached_fs_page('etc-hosts.md', '/etc/hosts is a file that contains the IP addresses')
 
     def test_get_page_for_absolute_path_directory(self):
         page = FSPathPage.get_page("/bin")
@@ -32,22 +59,10 @@ class TestFSPathPage(unittest.TestCase):
         self.assertFalse(page.description() == "no page found")
 
     def test_get_page_for_relative_path_in_variable_dir(self):
-        trie = GlobTrie()
-        trie.add('/home/*/.bashrc', 'globbashrc.md')
-        page_content = 'bashrc description'
-
-        with Patcher() as patcher:
-            patcher.fs.create_file("/home/someUser/.bashrc", contents="test")
-            patcher.fs.create_file("/home/someUser/.cache/wat/fs_pages/index.json", 
-            contents=trie.store_string())
-            patcher.fs.create_file("/home/someUser/.cache/wat/fs_pages/globbashrc.md", 
-            contents=page_content)
-
-            with patch.object(FileCache, 'cache_dir_path', return_value=pathlib.Path('/home/someUser/.cache/wat')):
-                os.chdir('/home/someUser')
-                page = FSPathPage.get_page(".bashrc")
-                self.assertTrue(page.path.as_posix() == "/home/someUser/.bashrc")
-                self.assertTrue(page.description() == page_content)
+        os.chdir('/home/someUser')
+        page = FSPathPage.get_page(".bashrc")
+        self.assertTrue(page.path.as_posix() == "/home/someUser/.bashrc")
+        self.assertTrue(page.description() == 'bashrc description')
 
     #
     # Page Type
--- tests/test_wat.py
+++ tests/test_wat.py
@@ -1,60 +1,59 @@
 from wat import wat
 from wat.pagesources import TLDRPage
 import mock
+import unittest
 
 
-def test_lookup_name_fs_page():
-    """
-    Does it produce the correct page for a directory by absolute path?
-    """
-    page = wat.lookup_page("/lib")
-    assert "/lib contains important dynamic libraries and kernel modules" in page.description()
+class WatTest(unittest.TestCase):
 
+    @unittest.skip("Requires installation to work first")
+    def test_lookup_name_fs_page(self):
+        """
+        Does it produce the correct page for a directory by absolute path?
+        """
+        page = wat.lookup_page("/lib")
+        self.assertTrue("/lib contains important dynamic libraries and kernel modules" in page.description())
 
-def test_lookup_path_results_in_fs_page():
-    """
-    Does it produce the correct page for a directory by absolute path?
-    """
-    page = wat.lookup_page("/lib")
-    assert type(page).__name__ == "FSPathPage"
+    @unittest.skip("Requires installation to work first")
+    def test_lookup_path_results_in_fs_page(self):
+        """
+        Does it produce the correct page for a directory by absolute path?
+        """
+        page = wat.lookup_page("/lib")
+        self.assertTrue(type(page).__name__ == "FSPathPage")
 
+    def test_lookup_service_results_in_combined_page(self):
+        """
+        Many services are documented by tldr, which provides better descriptions.
+        """
+        page = wat.lookup_page("systemd-sysctl")
+        self.assertTrue(type(page).__name__ == "CombinedPage")
 
-def test_lookup_service_results_in_combined_page():
-    """
-    Many services are documented by tldr, which provides better descriptions.
-    """
-    page = wat.lookup_page("systemd-sysctl")
-    assert type(page).__name__ == "CombinedPage"
+    def test_lookup_builtin_results_in_combined_page(self):
+        """
+        Bash builtins are mostly documented by tldr, which provides better descriptions.
+        """
+        page = wat.lookup_page("echo")
+        self.assertTrue(type(page).__name__ == "CombinedPage")
 
+    def test_lookup_tldr_page(self):
+        """
+        Does it produce the correct page for a tldr page?
+        """
+        page = wat.lookup_page("ac")
+        self.assertTrue(type(page).__name__ == "TLDRPage")
 
-def test_lookup_builtin_results_in_combined_page():
-    """
-    Bash builtins are mostly documented by tldr, which provides better descriptions.
-    """
-    page = wat.lookup_page("echo")
-    assert type(page).__name__ == "CombinedPage"
+    def test_lookup_package_manager_page(self):
+        """
+        Does it produce the correct page for a package?
+        """
+        page = wat.lookup_page("zeitgeist")
+        self.assertTrue(type(page).__name__ == "PackageManagerPage")
 
-
-def test_lookup_tldr_page():
-    """
-    Does it produce the correct page for a tldr page?
-    """
-    page = wat.lookup_page("ac")
-    assert type(page).__name__ == "TLDRPage"
-
-
-def test_lookup_package_manager_page():
-    """
-    Does it produce the correct page for a package?
-    """
-    page = wat.lookup_page("zeitgeist")
-    assert type(page).__name__ == "PackageManagerPage"
-
-
-def test_update_page_sources():
-    """
-    Update the page sources.
-    """
-    with mock.patch.object(TLDRPage, 'update_page_source') as mock_update:
-        wat.update_page_sources()
-        mock_update.assert_called_once_with()
+    def test_update_page_sources(self):
+        """
+        Update the page sources.
+        """
+        with mock.patch.object(TLDRPage, 'update_page_source') as mock_update:
+            wat.update_page_sources()
+            mock_update.assert_called_once_with()
